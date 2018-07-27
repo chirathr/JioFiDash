bbe430e1836a308029286bbcc6f3df0f43b785f3
Chirath R <chirath.02@gmail.com>
2018-07-28 00:06:15 +0530

    Placeholder function that gets username and password from user

--- app/src/main/java/com/chirathr/jiofidash/data/JioFiPreferences.java
+++ app/src/main/java/com/chirathr/jiofidash/data/JioFiPreferences.java
@@ -1,4 +1,21 @@
 package com.chirathr.jiofidash.data;
 
+import android.content.Context;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class JioFiPreferences {
+
+    public static final String USERNAME_STRING_ID = "username";
+    public static final String PASSWORD_STRING_ID = "password";
+
+    public static Map<String, String> getUserLoginData(Context context) {
+        Map<String,String> params = new HashMap<String, String>();
+
+        params.put(USERNAME_STRING_ID, "chirath");
+        params.put(PASSWORD_STRING_ID, "Rchirath02");
+
+        return params;
+    }
 }
