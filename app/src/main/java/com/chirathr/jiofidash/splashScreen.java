56d1beaff1cf70e0f987aca510df33700ab99a3e
Chirath R <chirath.02@gmail.com>
2018-07-31 17:35:28 +0530

    Added animated laucher icon

---
+++ app/src/main/java/com/chirathr/jiofidash/splashScreen.java
@@ -0,0 +1,14 @@
+package com.chirathr.jiofidash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
