b43aff1691328387c9bfc4614b25827a07398e92
Chirath R <chirath.02@gmail.com>
2018-08-01 19:36:12 +0530

    Singleton for network requests

---
+++ app/src/main/java/com/chirathr/jiofidash/fragments/DataSpeedCardFragment.java
@@ -0,0 +1,4 @@
+package com.chirathr.jiofidash.fragments;
+
+public class DataSpeedCardFragment {
+}
---
+++ app/src/main/java/com/chirathr/jiofidash/fragments/LteCardFragment.java
@@ -0,0 +1,6 @@
+package com.chirathr.jiofidash.fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class LteCardFragment extends Fragment {
+}
