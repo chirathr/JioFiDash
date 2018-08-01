d97c7a4c810d5c1c27df6df282c6e8e3dba5710d
Chirath R <chirath.02@gmail.com>
2018-08-01 22:11:42 +0530

    Made some constands public

--- app/src/main/java/com/chirathr/jiofidash/data/JioFiData.java
+++ app/src/main/java/com/chirathr/jiofidash/data/JioFiData.java
@@ -14,11 +14,11 @@ import java.util.List;
 public class JioFiData {
 
     // Lte info
-    private static final String LTE_TIME_STRING = "time_str";
-    private static final String LTE_STATUS = "status";
-    private static final String LTE_BAND = "opband";
-    private static final String LTE_BANDWIDTH = "bandwidth";
-    private static final String LTE_PHYSICAL_CELL_ID = "pcellID";
+    public static final String LTE_TIME_STRING = "time_str";
+    public static final String LTE_STATUS = "status";
+    public static final String LTE_BAND = "opband";
+    public static final String LTE_BANDWIDTH = "bandwidth";
+    public static final String LTE_PHYSICAL_CELL_ID = "pcellID";
 
     public String lteTimeString;
     public String lteStatus;
@@ -27,10 +27,10 @@ public class JioFiData {
     public String lteCellId;
 
     // Performance
-    private static final String UPLOAD_RATE = "txRate";
-    private static final String UPLOAD_RATE_MAX = "txmax";
-    private static final String DOWNLOAD_RATE = "rxRate";
-    private static final String DOWNLOAD_RATE_MAX = "rxmax";
+    public static final String UPLOAD_RATE = "txRate";
+    public static final String UPLOAD_RATE_MAX = "txmax";
+    public static final String DOWNLOAD_RATE = "rxRate";
+    public static final String DOWNLOAD_RATE_MAX = "rxmax";
 
     public String uploadRateString;
     public String uploadRateMaxString;
@@ -38,8 +38,8 @@ public class JioFiData {
     public String downloadRateMaxString;
 
     // Lan information (connected users)
-    private static final String USER_COUNT = "act_cnt";
-    private static final String USER_LIST = "userlistinfo";
+    public static final String USER_COUNT = "act_cnt";
+    public static final String USER_LIST = "userlistinfo";
 
     public int userCount;
     public List<String> userNameList;
@@ -47,22 +47,22 @@ public class JioFiData {
     public List<String> userMacList;
 
     // Wan information (total data used)
-    private static final String TOTAL_UPLOAD = "duration_ul";
-    private static final String TOTAL_DOWNLOAD = "duration_dl";
+    public static final String TOTAL_UPLOAD = "duration_ul";
+    public static final String TOTAL_DOWNLOAD = "duration_dl";
 
     public String totalUploadString;
     public String totalDownloadString;
 
     // Device Info
-    private static final String BATTERY_LEVEL = "batterylevel";
-    private static final String BATTERY_STATUS = "batterystatus";
+    public static final String BATTERY_LEVEL = "batterylevel";
+    public static final String BATTERY_STATUS = "batterystatus";
 
     public int batteryLevel;
     public String batteryStatus;
 
     private final String TAG = JioFiData.class.getSimpleName();
 
-    private static int toInt(String value) {
+    public static int toInt(String value) {
         try {
             return Integer.parseInt(value);
         } catch (Exception e) {
@@ -70,7 +70,7 @@ public class JioFiData {
         }
     }
 
-    private static double toDouble(String value) {
+    public static double toDouble(String value) {
         try {
             return Double.parseDouble(value);
         } catch (Exception e) {
