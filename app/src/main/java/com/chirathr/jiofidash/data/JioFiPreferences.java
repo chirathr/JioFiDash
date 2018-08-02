e182bae8202ba25a830a7828f805643bd9e64dbc
Chirath R <chirath.02@gmail.com>
2018-08-02 22:33:03 +0530

    System to save and load password

--- app/src/main/java/com/chirathr/jiofidash/data/JioFiPreferences.java
+++ app/src/main/java/com/chirathr/jiofidash/data/JioFiPreferences.java
@@ -1,6 +1,9 @@
 package com.chirathr.jiofidash.data;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.chirathr.jiofidash.R;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,12 +13,68 @@ public class JioFiPreferences {
     public static final String USERNAME_STRING_ID = "username";
     public static final String PASSWORD_STRING_ID = "password";
 
-    public static Map<String, String> getUserLoginData(Context context) {
+    private static JioFiPreferences mInstance;
+
+    public String username;
+    public String password;
+
+    public Map<String, String> getUserLoginData(Context context) {
         Map<String,String> params = new HashMap<String, String>();
 
-        params.put(USERNAME_STRING_ID, "chirath");
-        params.put(PASSWORD_STRING_ID, "Rchirath02");
+        if (username == null || password == null) {
+            boolean didLoad = loadUsernameAndPassword(context);
+            if (!didLoad)
+                return null;
+        }
+        params.put(USERNAME_STRING_ID, username);
+        params.put(PASSWORD_STRING_ID, password);
 
         return params;
+    }
+
+    public static synchronized JioFiPreferences getInstance() {
+        if (mInstance == null)
+            mInstance = new JioFiPreferences();
+        return mInstance;
+    }
+
+    public void setUsernameAndPassword(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // TODO load from shared preference
+    public void saveUsernameAndPassword(Context context) {
+
+        if (username == null || password == null) {
+            return;
+        }
+
+        SharedPreferences sharedPref = context.getSharedPreferences(
+                context.getString(R.string.data_preference_file_key), Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putString(context.getString(R.string.saved_user_name_key), username);
+        editor.putString(context.getString(R.string.saved_password_key), password);
+
+        editor.apply();
+    }
+
+    public boolean loadUsernameAndPassword(Context context) {
+
+        SharedPreferences sharedPref = context.getSharedPreferences(
+                context.getString(R.string.data_preference_file_key), Context.MODE_PRIVATE);
+
+        if (sharedPref.contains(context.getString(R.string.saved_user_name_key))
+                && sharedPref.contains(context.getString(R.string.saved_password_key))) {
+
+            username = sharedPref.getString(context.getString(R.string.saved_user_name_key), null);
+            password = sharedPref.getString(context.getString(R.string.saved_password_key), null);
+
+            return true;
+        }
+
+        return false;
     }
 }
