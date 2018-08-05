c910b98008797165a53ca00a7112aa8a77afe64a
Chirath R <chirath.02@gmail.com>
2018-08-05 15:23:13 +0530

    bottom sheet fragment

--- app/src/main/java/com/chirathr/jiofidash/fragments/BottomSheetFragment.java
+++ app/src/main/java/com/chirathr/jiofidash/fragments/BottomSheetFragment.java
@@ -1,9 +1,11 @@
 package com.chirathr.jiofidash.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.chirathr.jiofidash.R;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
@@ -12,9 +14,45 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class BottomSheetFragment extends BottomSheetDialogFragment {
+
+    public static final int OPTION_RESTART_ID = 0;
+    public static final int OPTION_WIFI_SETTINGS_ID = 1;
+    public static final int OPTION_SETTINGS_ID = 2;
+    public static final int OPTION_ABOUT_ID = 3;
+
+    private onOptionSelectedListener mListener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            mListener = (onOptionSelectedListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement onOptionSelectedListener");
+        }
+    }
+
+    private TextView restartTextView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.bottom_sheet_fragment_card, container, false);
+
+        View view = inflater.inflate(R.layout.bottom_sheet_fragment_card, container, false);
+
+        restartTextView = (TextView) view.findViewById(R.id.action_restart);
+
+        restartTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mListener.onOptionSelected(OPTION_RESTART_ID);
+            }
+        });
+
+        return view;
+    }
+
+    public interface onOptionSelectedListener {
+        void onOptionSelected(int optionId);
     }
 }
