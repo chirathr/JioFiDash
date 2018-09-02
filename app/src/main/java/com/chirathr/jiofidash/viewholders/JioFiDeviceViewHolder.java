fd3ffde7b76dd5daebd6ccde732cd46e6ccb4ea7
Chirath R <chirath.02@gmail.com>
2018-09-02 08:17:06 +0530

    JioFi device view holder

---
+++ app/src/main/java/com/chirathr/jiofidash/viewholders/JioFiDeviceViewHolder.java
@@ -0,0 +1,47 @@
+package com.chirathr.jiofidash.viewholders;
+
+import android.content.res.Resources;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.chirathr.jiofidash.R;
+import com.chirathr.jiofidash.adapters.JioFiDeviceListAdapter;
+import com.chirathr.jiofidash.data.JioFiDeviceViewModel;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class JioFiDeviceViewHolder extends RecyclerView.ViewHolder {
+
+    private JioFiDeviceListAdapter.JiofiDeviceOnClickListner mListener;
+
+    private ImageView deviceImageView;
+    private TextView deviceNameTextView;
+    private TextView descriptionTextView;
+    private Button deviceSelectButton;
+
+    public JioFiDeviceViewHolder(@NonNull View itemView) {
+        super(itemView);
+        deviceImageView = itemView.findViewById(R.id.jiofiDeviceImage);
+        deviceNameTextView = itemView.findViewById(R.id.jioFiName);
+        descriptionTextView = itemView.findViewById(R.id.jioFiDescription);
+        deviceSelectButton = itemView.findViewById(R.id.jioFiSelectButton);
+    }
+
+    public void bind(JioFiDeviceViewModel viewModel, JioFiDeviceListAdapter.JiofiDeviceOnClickListner listener) {
+        mListener = listener;
+
+        deviceImageView.setImageDrawable(viewModel.getDeviceImage());
+        deviceNameTextView.setText(viewModel.getDeviceName());
+        descriptionTextView.setText(viewModel.getDescription());
+
+        if (viewModel.isSupported()) {
+            deviceSelectButton.setText(R.string.select_button_text);
+        } else {
+            deviceSelectButton.setText(R.string.web_ui_button_text);
+        }
+
+    }
+}
