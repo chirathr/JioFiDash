f7d8558ec2bbb371c241c4fbc9cbb0d267bc57b7
Chirath R <chirath.02@gmail.com>
2018-09-02 09:35:28 +0530

    Layour for onBoarding

--- app/src/main/java/com/chirathr/jiofidash/data/JioFiDevicesData.java
+++ app/src/main/java/com/chirathr/jiofidash/data/JioFiDevicesData.java
@@ -11,13 +11,39 @@ public class JioFiDevicesData {
             6, R.drawable.jiofi6, "JioFi 6 (JMR815)",
             "JioFi 6(JMR815) 4G device with 3000mah battery, WiFi and micro SD.", true);
 
+    private static final JioFiDeviceViewModel jioFiM2S = new JioFiDeviceViewModel(
+            7, R.drawable.jiofi6, "JioFi M2S",
+            "JioFi 6(JMR815) 4G device with 3000mah battery, WiFi and micro SD.", false);
+
     private static final JioFiDeviceViewModel jioFi5 = new JioFiDeviceViewModel(
-            6, R.drawable.jiofi6, "JioFi 5 (JMR814)",
+            5, R.drawable.jiofi6, "JioFi 5 (JMR814)",
+            "JioFi 6(JMR815) 4G device with 3000mah battery, WiFi and micro SD.", false);
+
+    private static final JioFiDeviceViewModel jioFi4 = new JioFiDeviceViewModel(
+            4, R.drawable.jiofi6, "JioFi 5 (JMR814)",
+            "JioFi 6(JMR815) 4G device with 3000mah battery, WiFi and micro SD.", false);
+
+    private static final JioFiDeviceViewModel jioFi3 = new JioFiDeviceViewModel(
+            3, R.drawable.jiofi6, "JioFi 5 (JMR814)",
+            "JioFi 6(JMR815) 4G device with 3000mah battery, WiFi and micro SD.", false);
+
+    private static final JioFiDeviceViewModel jioFi2 = new JioFiDeviceViewModel(
+            2, R.drawable.jiofi6, "JioFi 5 (JMR814)",
+            "JioFi 6(JMR815) 4G device with 3000mah battery, WiFi and micro SD.", false);
+
+    private static final JioFiDeviceViewModel jioFi1 = new JioFiDeviceViewModel(
+            1, R.drawable.jiofi6, "JioFi 5 (JMR814)",
             "JioFi 6(JMR815) 4G device with 3000mah battery, WiFi and micro SD.", false);
 
     public static List<JioFiDeviceViewModel> getDevices() {
         List<JioFiDeviceViewModel> deviceViewModels = new ArrayList<>();
         deviceViewModels.add(jioFi6);
+        deviceViewModels.add(jioFiM2S);
+        deviceViewModels.add(jioFi5);
+        deviceViewModels.add(jioFi4);
+        deviceViewModels.add(jioFi3);
+        deviceViewModels.add(jioFi2);
+        deviceViewModels.add(jioFi1);
 
         return deviceViewModels;
     }
